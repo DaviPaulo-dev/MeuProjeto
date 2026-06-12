b7cf576f73a605f0d84a3a02f2dbecfddaddcfe7
DaviPaulo-dev <davipaulo.dev@gmail.com>
2026-06-12 16:40:27 -0300

    mensagem

---
+++ SRC/js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll", () => {
+    let header = document.getElementById("header");
+    let scrollTop = window.scrollY;
+    let maxScroll = document.body.scrollHeight - window.innerHeight;
+    let opacity = Math.max(0, 1 - (scrollTop / maxScroll));
+    header.style.opacity = opacity;
+});
+
+const btnTopo = document.getElementById("BtTopo");
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 300){
+        btnTopo.style.display = "block";
+    }else{
+        btnTopo.style.display = "none";
+    }
+
+});
+
+btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
